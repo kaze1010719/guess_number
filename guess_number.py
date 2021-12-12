3a5f473bed938ab235a68ee5798a5910c66a1167
Isis <kaze1010719@gmail.com>
2021-12-12 13:53:11 +0800

    basic version

---
+++ guess_number.py
@@ -0,0 +1,17 @@
+import random
+
+r = random.randint(1, 100)
+
+while True:
+	num = input('請猜數字1~100: ')
+	num = int(num)
+	if num == r:
+		print('終於猜對了!')
+		break
+	elif num > r:
+		print('比答案大')
+	else:
+		print('比答案小')
+
+
+
